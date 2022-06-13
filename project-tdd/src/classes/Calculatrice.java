c023a476322d77e2c89d9463e480a6a590c85a85
Thibaut <tibeloute@msn.com>
2022-06-14 00:38:21 +0200

    Ajout de méthodes : add soustraction mul et div

--- project-tdd/src/classes/Calculatrice.java
+++ project-tdd/src/classes/Calculatrice.java
@@ -3,7 +3,19 @@ package classes;
 public class Calculatrice {
 
 	public int add(int a, int b) {
-		return a+b;
+		return a + b;
 	}
-	
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int mul(int a, int b) {
+		return a * b;
+	}
+
+	public int div(int a, int b) {
+		return a / b;
+	}
+
 }
--- project-tdd/src/tests/CalculatriceTest.java
+++ project-tdd/src/tests/CalculatriceTest.java
@@ -11,12 +11,68 @@ class CalculatriceTest {
 
 	@Test
 	public void testAdd() {
-		Calculatrice c = new Calculatrice(); 
+		Calculatrice c = new Calculatrice();
 		int aInput = 8;
 		int bInput = 5;
 		int valeurAttendu = 13;
 		// passé le asserEquals en méthode statique
-		assertEquals("Le résultat doit retourner une somme", valeurAttendu, c.add(aInput, bInput)); 
+		assertEquals("Le résultat doit retourner une somme", valeurAttendu, c.add(aInput, bInput));
 	}
 
+	@Test
+	public void testsub() {
+		Calculatrice c = new Calculatrice();
+		int aInput = 8;
+		int bInput = 5;
+		int valeurAttendu = 3;
+		// passé le asserEquals en méthode statique
+		assertEquals("Le résultat doit retourner une soustraction", valeurAttendu, c.sub(aInput, bInput));
+	}
+
+	@Test
+	public void testMul() {
+		Calculatrice c = new Calculatrice();
+		int aInput = 8;
+		int bInput = 5;
+		int valeurAttendu = 40;
+		// passé le asserEquals en méthode statique
+		assertEquals("Le résultat doit retourner une multiplication", valeurAttendu, c.mul(aInput, bInput));
+	}
+
+	@Test
+	public void testDiv() {
+		Calculatrice c = new Calculatrice();
+		int aInput = 30;
+		int bInput = 10;
+		int valeurAttendu = 3;
+		// passé le asserEquals en méthode statique
+		assertEquals("Le résultat doit retourner une division", valeurAttendu, c.div(aInput, bInput));
+	}
+	
+//	@Test
+//	(expected=ArithmeticException.class)
+//	public void testDivision2() {
+//		Calculatrice c = new Calculatrice();
+//		int aInput = 10;
+//		int bInput = 0;
+//		c.div(aInput, bInput);		
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
